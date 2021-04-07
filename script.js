b5622a88d09fd6fc5904fa11f61ce043be724479
jmasudd <jmasudd@gmail.com>
2021-04-07 21:20:07 +0600

    Updated to latest

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var _ = require('lodash')
+var array = [1,1,2,3,3,4,5,6,7]
+console.log('answer' ,_.without(array,3))
 var body = document.getElementById('BGColor')
 var color1 = document.querySelector('.color1')
 var color2 = document.querySelector('.color2')
